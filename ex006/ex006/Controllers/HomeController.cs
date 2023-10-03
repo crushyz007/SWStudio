4aef52e9a593eb5977980e9fcbd0adab3254d68d
crushyz007 <gracetyn2002@gmail.com>
2023-10-03 20:50:59 +0700

    ex006

---
+++ ex006/ex006/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ex006.Models;
+
+namespace ex006.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    
+        public IActionResult Answer()
+        {
+            string fname = Request.Form["fname"];
+            string lname = Request.Form["lname"];
+            string email = Request.Form["email"];
+
+            string ans = Request.Form["ans"];
+
+            string textarea = Request.Form["textarea"];
+            
+            ViewBag.fname = fname;
+            ViewBag.lname = lname;
+            ViewBag.email = email;
+            
+            ViewBag.ans = ans;
+
+            ViewBag.textarea = textarea;
+
+            return View();
+        }
+
+    }
+}
